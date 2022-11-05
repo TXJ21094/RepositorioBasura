dfe649ff504eb380fe6cc4ac5e980dc091154e3a
pablold <pablold@PAVANE_HP>
2022-11-05 17:22:37 +0100

    Subo la calse dentro de paquete

---
+++ ProyectoBasura/src/estudiantes/MainTXJ21.java
@@ -0,0 +1,10 @@
+package estudiantes;
+
+public class MainTXJ21 {
+
+	public static void main(String[] args) {
+		System.out.println("Hola! Soy Pablo López Díaz (TXJ21094@educastur.es)");
+
+	}
+
+}
