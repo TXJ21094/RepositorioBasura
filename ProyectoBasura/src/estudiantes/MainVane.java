67bebd3f9371ffbe155f0a645db377176c63f7cb
pablold <pablold@PAVANE_HP>
2022-11-05 17:30:20 +0100

    segunda subida

--- ProyectoBasura/src/estudiantes/MainVane.java
+++ ProyectoBasura/src/estudiantes/MainVane.java
@@ -3,7 +3,7 @@ package estudiantes;
 public class MainVane {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Hola! Soy Vanesa Iglesias Fernandez (vaneigf@gmail.com)");
 
 	}
 
